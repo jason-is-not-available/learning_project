855fedf2b0455f8db55c1df79c690f4817494f7c
“Jason <believejason@gmail.com>
2023-07-14 17:31:05 -0400

    stuck on making data types work

--- liquor.go
+++ liquor.go
@@ -3,10 +3,12 @@
 package main
 
 import (
+	"container/list"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"reflect"
 )
 
 type response1 struct {
@@ -15,6 +17,7 @@ type response1 struct {
 }
 
 // Probably dont want a struct for this, but we'll start here
+// might actually have use for it
 type sLiquors struct {
 	Type   []string
 	Amount int
@@ -39,8 +42,8 @@ func hello(w http.ResponseWriter, req *http.Request) {
 		Page:   1,
 		Fruits: []string{"apple", "peach", "pear"}}
 
-	reqJson, _ := json.Marshal(req)
-	fmt.Fprintf(w, string(reqJson))
+	//reqJson, _ := json.Marshal(req)
+	//fmt.Fprintf(w, string(reqJson))
 
 	fmt.Printf(req.Method)
 
@@ -48,6 +51,64 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, string(reqDump))
 
 	pageJson, _ := json.Marshal(page)
+	fmt.Fprintf(w, string(pageJson))
+
+	const menuLength int = 2
+}
+
+// Returns the entire map of liquors and their amounts
+// in JSON format.
+func liquors(w http.ResponseWriter, req *http.Request) {
+
+	// make an array of structs from the map
+	// then marshal that array
+
+	// This should be a global int?
+	// var arr [menuLength]sLiquors
+	var arr [2]sLiquors
+
+	// for i := 0; i < len(arr); i++ {
+	// 	// ..cant iterate a map via index..
+	// }
+
+	for key, value := range mLiquors {
+		i := 0
+		// arr[i].Type = key
+		// arr[i].Type = fmt.Sprintf(key)
+		// Key is not a string and does not want to become one?
+		// arr[i].Amount, _ = strconv.Atoi(value)
+		arr[i].Amount = value
+		fmt.Printf(key)
+		// fmt.Printf(value)
+		i++
+	}
+
+	myList := list.New()
+	for key, value := range mLiquors {
+		item := &sLiquors{
+			Type:   []string{key},
+			Amount: value,
+		}
+		fmt.Println(reflect.TypeOf(item))
+
+		myList.PushBack(item)
+	}
+
+	one := myList.Front()
+	fmt.Println(reflect.TypeOf(one))
+
+	// var len = len(mLiquors)
+
+	// var arr [len]sLiquors
+
+	// for k, v := range mLiquors {
+	// 	arr = append(arr, k, v)
+	// }
+	/*
+		fmt.Println("Array ended up being: ")
+		ftm.Println(arr) */
+
+	pageJson, _ := json.Marshal(mLiquors)
 	fmt.Fprintf(w, string(pageJson))
 
 }
@@ -61,6 +122,8 @@ func main() {
 	// the *default router* in the `net/http` package and
 	// takes a function as an argument.
 	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/liquors", liquors)
 
 	// Finally, we call the `ListenAndServe` with the port
 	// and a handler. `nil` tells it to use the default
