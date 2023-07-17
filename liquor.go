2d9ff80a8644907330b6167f7c72615905600d69
“Jason <believejason@gmail.com>
2023-07-16 21:05:46 -0400

    Starting to write my own handler

--- liquor.go
+++ liquor.go
@@ -6,8 +6,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
+	"strings"
 )
-
 
 // Probably dont want a struct for this, but we'll start here
 // might actually have use for it
@@ -18,11 +19,40 @@ type sLiquors struct {
 
 var mLiquors = map[string]int{"bourbon": 8, "vodka": 2}
 
-// Returns the entire map of liquors and their amounts
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("We got to my handler")
+
+	endpoint := req.URL.Path
+	method := req.Method
+
+	fmt.Println("Method: ", method)
+
+	// Lowercase and remove trailing "/" if it exists
+	endpoint = strings.ToLower(endpoint)
+	if strings.HasSuffix(endpoint, "/") {
+		endpoint = endpoint[:len(endpoint)-1]
+	}
+
+	fmt.Println("testing slice", len(endpoint))
+	fmt.Println(endpoint[:8])
+
+	if len(endpoint) == 8 && endpoint[:8] == "/liquors" {
+		fmt.Println("OMG LIQUOUROURSS")
+	}
+
+	switch endpoint {
+	case "/liquors":
+		fmt.Println("Case liquors")
+
+	}
+
+}
+
+// Returns the entire map of liquors and their quantities
 // in JSON format.
 func liquors(w http.ResponseWriter, req *http.Request) {
 
-	var liquorArray []sLiquors
+	var liquorSlice []sLiquors
 
 	//myLiquors := list.New()
 	for key, value := range mLiquors {
@@ -30,24 +60,36 @@ func liquors(w http.ResponseWriter, req *http.Request) {
 			Type:   key,
 			Amount: value,
 		}
-		liquorArray = append(liquorArray, item)
+		liquorSlice = append(liquorSlice, item)
 		//fmt.Println(item)
 		//testJson, _ := json.Marshal(item)
 		//fmt.Println(string(testJson))
 		//myLiquors.PushBack(item)
 	}
 
-	fmt.Println(liquorArray)
+	// This seems to do noting
+	fmt.Println(liquorSlice)
 
-	liquorsJson, _ := json.Marshal(liquorArray)
-	//fmt.Println(string(liquorsJson))
+	println(req.URL.Path)
+
+	whatis := req.URL.Path
+	println(whatis)
+	println(reflect.TypeOf(liquorSlice).String())
+
+	liquorsJson, _ := json.Marshal(liquorSlice)
+
 	fmt.Fprintf(w, string(liquorsJson))
 
 }
 
 func main() {
 
-	http.HandleFunc("/liquors", liquors)
+	// Identify the endpoint and decide what to do with it
+
+	http.HandleFunc("/", myHandler)
+
+	// Keep this around
+	// http.HandleFunc("/liquors", liquors)
 
 	// Finally, we call the `ListenAndServe` with the port
 	// and a handler. `nil` tells it to use the default
